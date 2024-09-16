0a70c4f91571a9f2eee333548555f18e21a8db74
YenNgoc939 <yenngocnguyen.210303@gmail.com>
2024-09-16 20:24:33 +0700

    Standardize JSON responses

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
-const express = require('express'); 
-const cors = require('cors'); 
-const contactsRouter = require('./routes/contacts.router');
-const app = express(); 
-app.use(cors()); 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-app.get('/', (req, res) => { 
-return res.json({ 
-message: 'ok' 
-}); 
-}); 
-contactsRouter.setup(app); 
-module.exports = app; 
+const express = require('express');
+const cors = require('cors');
+const JSend = require('./jsend');
+const contactsRouter = require('./routes/contacts.router'); // Tách dòng
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  return res.json(JSend.success());
+});
+
+contactsRouter.setup(app);
+
+module.exports = app;
--- src/routes/contacts.router.js
+++ src/routes/contacts.router.js
@@ -1,11 +1,17 @@
-const express = require('express'); 
-const contactsController = require('../controllers/contacts.controller'); const router = express.Router(); 
-module.exports.setup = (app) => { 
-app.use('/api/v1/contacts', router); 
-router.get('/', contactsController.getContactsByFilter); 
-router.post('/', contactsController.createContact); 
-router.delete('/', contactsController.deleteAllContacts); 
-router.get('/:id', contactsController.getContact); 
-router.put('/:id', contactsController.updateContact); 
-router.delete('/:id', contactsController.deleteContact); 
-}; 
+const express = require('express');
+const contactsController = require('../controllers/contacts.controller');
+
+
+const router = express.Router();
+
+router.get('/', contactsController.getContactsByFilter);
+router.post('/', contactsController.createContact);
+router.delete('/', contactsController.deleteAllContacts);
+
+
+router.get('/:id', contactsController.getContact);
+router.put('/:id', contactsController.updateContact); // Add this line for PUT /api/v1/contacts/{id}
+router.delete('/:id', contactsController.deleteContact);
+
+
+module.exports = { setup: (app) => app.use('/api/v1/contacts', router) };
